7365501c48a7a51878d78a4b3b110f9781c2b9c7
Vladimir Berg <vladimir.berg@simbirsoft.com>
2026-04-28 10:54:40 +0300

    Add gRPC client, server and test files

---
+++ grpc/client.py
@@ -0,0 +1,15 @@
+﻿import grpc
+import grpc.hello_pb2 as pb2
+import grpc.hello_pb2_grpc as pb2_grpc
+
+class GrpcClient:
+    def __init__(self, host=""localhost:50051""):
+        self.channel = grpc.insecure_channel(host)
+        self.stub = pb2_grpc.GreeterStub(self.channel)
+
+    def say_hello(self, name: str) -> str:
+        response = self.stub.SayHello(pb2.HelloRequest(name=name))
+        return response.message
+
+    def close(self):
+        self.channel.close()
---
+++ grpc/tests/test_grpc.py
@@ -0,0 +1,15 @@
+﻿import pytest
+import allure
+from grpc.client import GrpcClient
+
+@allure.feature(""gRPC"")
+class TestGrpc:
+
+    @allure.title(""gRPC SayHello returns greeting"")
+    @pytest.mark.grpc
+    def test_say_hello(self):
+        client = GrpcClient()
+        result = client.say_hello(""QA"")
+        assert ""Hello"" in result
+        assert ""QA"" in result
+        client.close()
